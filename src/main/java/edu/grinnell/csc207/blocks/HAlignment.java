ad8257f438578adf1a5ee516d2c566772132d422
David Stroud <>
2024-09-20 16:02:55 -0500

    Add Alignment utility function

--- src/main/java/edu/grinnell/csc207/blocks/HAlignment.java
+++ src/main/java/edu/grinnell/csc207/blocks/HAlignment.java
@@ -9,5 +9,36 @@ public enum HAlignment {
   /** Align by the center column. */
   CENTER,
   /** Align by the right column. */
-  RIGHT
+  RIGHT;
+
+  /**
+   * Returns the location of a column in the inner block.
+   * @param col The column of the outer block.
+   * @param innerWidth The width of the block that needs to be aligned.
+   * @param outerWidth The width of the block that is aligning.
+   * @return The equivalent column of the inner block, or -1 if the column is not in the inner block.
+   */
+  public int getLocation(int col, int innerWidth, int outerWidth) {
+    int diff = outerWidth - innerWidth;
+
+    if (this == HAlignment.LEFT && col >= innerWidth) {
+      return -1;
+    } else if (this == HAlignment.LEFT && col < innerWidth) {
+      return col;
+    } else if (this == HAlignment.RIGHT && col < diff) {
+      return -1;
+    } else if (this == HAlignment.RIGHT && col >= diff) {
+      return col - outerWidth + innerWidth;
+    } else {
+      // We have HAlignment.CENTER
+      int leftSide = diff / 2;
+      int rightSide = diff - leftSide;
+
+      if (col >= leftSide && col < (outerWidth - rightSide)) {
+        return col - leftSide;
+      } else {
+        return -1;
+      } // if-else
+    } // if-else
+  } // getLocation(int, int, int)
 };
--- src/main/java/edu/grinnell/csc207/blocks/VAlignment.java
+++ src/main/java/edu/grinnell/csc207/blocks/VAlignment.java
@@ -9,6 +9,37 @@ public enum VAlignment {
   /** Align to the center. */
   CENTER,
   /** Align to the bottom. */
-  BOTTOM
+  BOTTOM;
+
+    /**
+   * Returns the location of a row in the inner block.
+   * @param row The row of the outer block.
+   * @param innerHeight The height of the block that needs to be aligned.
+   * @param outerHeight The height of the block that is aligning.
+   * @return The equivalent row of the inner block, or -1 if the row is not in the inner block.
+   */
+  public int getLocation(int row, int innerHeight, int outerHeight) {
+    int diff = outerHeight - innerHeight;
+
+    if (this == VAlignment.TOP && row >= innerHeight) {
+      return -1;
+    } else if (this == VAlignment.TOP && row < innerHeight) {
+      return row;
+    } else if (this == VAlignment.BOTTOM && row < diff) {
+      return -1;
+    } else if (this == VAlignment.BOTTOM && row >= diff) {
+      return row - outerHeight + innerHeight;
+    } else {
+      // We have VAlignment.CENTER
+      int topSide = diff / 2;
+      int bottomSide = diff - topSide;
+
+      if (row >= topSide && row < (outerHeight - bottomSide)) {
+        return row - topSide;
+      } else {
+        return -1;
+      } // if-else
+    } // if-else
+  } // getLocation(int, int, int)
 } // enum VAlignment
 
