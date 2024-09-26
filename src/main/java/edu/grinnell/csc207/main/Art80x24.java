f615549c0f3c8923e4a7d3d5a808ef28d8a86271
sarah_deschamps <deschamps.sias@gmail.com>
2024-09-25 21:36:15 -0500

    Ascii Art Complete.

--- src/main/java/edu/grinnell/csc207/main/Art80x24.java
+++ src/main/java/edu/grinnell/csc207/main/Art80x24.java
@@ -2,6 +2,17 @@ package edu.grinnell.csc207.main;
 
 import edu.grinnell.csc207.blocks.AsciiBlock;
 import edu.grinnell.csc207.blocks.Rect;
+import edu.grinnell.csc207.blocks.HComp;
+import edu.grinnell.csc207.blocks.VComp;
+import edu.grinnell.csc207.blocks.HAlignment;
+import edu.grinnell.csc207.blocks.VAlignment;
+import edu.grinnell.csc207.blocks.Grid;
+import edu.grinnell.csc207.blocks.Line;
+import edu.grinnell.csc207.blocks.Boxed;
+
+
+
+
 
 import java.io.PrintWriter;
 
@@ -23,7 +34,51 @@ public class Art80x24 {
    */
   public static void main(String[] args) throws Exception {
     PrintWriter pen = new PrintWriter(System.out, true);
-    AsciiBlock art = new Rect('^', 80, 24);
+    // AsciiBlock art = new Rect('^', 80, 24);
+    AsciiBlock picnicGrid = new Rect('/', 3, 2);
+    AsciiBlock emptyPicnicGrid = new Rect(' ', 3, 2);
+    AsciiBlock blanketPart = new VComp(HAlignment.CENTER,
+                                       new HComp(VAlignment.CENTER, picnicGrid, emptyPicnicGrid),
+                                       new HComp(VAlignment.CENTER, emptyPicnicGrid, picnicGrid));
+    AsciiBlock blanket = new Grid(blanketPart, 13, 2);
+    AsciiBlock[] flowerLines = new AsciiBlock[] {new Line("(\\/\\)"),
+                                                 new Line("(*)  \\_/  (*)"),
+                                                 new Line("\\   |  /"),
+                                                 new Line("\\  | |"),
+                                                 new Line("+-------+"),
+                                                 new Line("|       |"),
+                                                 new Line("{         }"),
+                                                 new Line("\\       /"),
+                                                 new Line("\\_____/")};
+    AsciiBlock flower = new VComp(HAlignment.CENTER, flowerLines);
+    AsciiBlock[] cakeLines = new AsciiBlock[] {new Line("*  *  *"),
+                                               new Line("|  |  |"),
+                                               new Line("-----------"),
+                                               new Line("|~~~~~~~~~|"),
+                                               new Line("|\\/\\/\\/\\/\\|"),
+                                               new Line("|_________|")};
+    AsciiBlock cake = new VComp(HAlignment.CENTER, cakeLines);
+    AsciiBlock items = new HComp(VAlignment.BOTTOM, new AsciiBlock[]
+      {new Rect(' ', 20, 9),
+        cake,
+      new Rect(' ', 20, 1),
+        flower});
+
+    AsciiBlock sun = new VComp(HAlignment.LEFT, new AsciiBlock[]
+      {new Line("########* -"),
+      new Line("########* /"),
+      new Line("#######*_\\"),
+      new Line("####* _\\"),
+      new Line("/ \\/")});
+    AsciiBlock cloud = new VComp(HAlignment.CENTER, new AsciiBlock[]
+      {new Line("_    ___"),
+      new Line("_( )__(   )_"),
+      new Line("_(            o__"),
+      new Line("o_________________)")});
+    AsciiBlock art = new Boxed(new VComp(HAlignment.LEFT, new AsciiBlock[]
+      {new HComp(VAlignment.BOTTOM, new AsciiBlock[]
+        {sun, new Rect(' ', 20, 1), cloud}),
+        items, blanket}));
     AsciiBlock.print(pen, art);
     pen.close();
   } // main(String[])
